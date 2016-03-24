c2562ebc4b1bdf9a6d93adf0bf1a9208197a5240
Alan Mooiman <amooiman@gmail.com>
2016-03-24 13:52:20 -0400

    Data, gitignore, react lifecycle

--- react/pages/timeline.jsx
+++ react/pages/timeline.jsx
@@ -1,9 +1,24 @@
 var React = require('react');
 
 var Timeline = React.createClass({
+  getDefaultProps: function(){
+    return {
+      data: 'foo'
+    };
+  },
+  getInitialState: function(){
+    return{
+      currentStoryIndex: 0
+    };
+  },
+  componentWillMount: function(){
+   //Make an AJAX call
+   this.AjaxResponse = 'foo'
+
+  },
   render: function(){
     return(
-      <div>Hello, world!</div>
+      <div>Hello, world! {this.AjaxResponse} {this.props.data} {this.state.currentStoryIndex}</div>
     );
   }
 });
